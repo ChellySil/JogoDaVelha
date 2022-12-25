faae8453e1cb79f8965308686fb0edc148e38a90
Michelle <109476419+ChellySil@users.noreply.github.com>
2022-12-25 16:04:58 -0300

    Add files via upload

---
+++ JogoDaVelha/src/JogoDaVelha/Jogo.java
@@ -0,0 +1,13 @@
+package JogoDaVelha;
+
+import java.util.Scanner;
+
+public class Jogo {
+	private Tabuleiro tabuleiro;
+	private int rodada = 1, vez =1;
+	private Jogador jogador1;
+	private Jogador jogador2;
+	private Scanner entrada = new Scanner(System.in);
+	
+	
+}
