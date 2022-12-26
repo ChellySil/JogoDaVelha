850d630ece762a0680b7121c00558828ec12f802
Michelle <109476419+ChellySil@users.noreply.github.com>
2022-12-26 12:14:49 -0300

    Add files via upload

---
+++ JogoDaVelha/src/JogoDaVelha/JogoDaVelha/Computador.java
@@ -0,0 +1,21 @@
+package JogoDaVelha;
+
+public class Computador extends Jogador {
+	public Computador(int jogador) {
+		super(jogador);
+		System.out.println("Jogador 'Computador' criado");
+	}
+	
+
+	@Override
+	public void jogar(Tabuleiro tabuleiro) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tentativa(Tabuleiro tabuleiro) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ JogoDaVelha/src/JogoDaVelha/JogoDaVelha/JogoDVelha.java
@@ -0,0 +1,8 @@
+package JogoDaVelha;
+
+
+public class JogoDVelha {
+	public static void main(String[] args) {
+		Jogo jogo = new Jogo();
+	}
+}
